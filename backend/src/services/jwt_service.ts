dd729e2a5993b37b7ac660ab777f84376f983742
Jeremy <jer.auriel@gmail.com>
2026-02-12 08:20:47 +0700

    feat(auth): jwt now returns role for future org/corp implementation

--- backend/src/services/jwt_service.ts
+++ backend/src/services/jwt_service.ts
@@ -1,5 +1,11 @@
 import jwt from "jsonwebtoken"
 
+interface JwtPayload {
+  id: string;
+  email: string;
+  role: 'org' | 'corp';
+}
+
 const jwtSecret = process.env.JWT_SECRET || 'blablabla1234saiwqjodnck';
 async function generateJWT(payload: { id: string; email: string; }): Promise<string> {
     const jwtToken = jwt.sign(payload, jwtSecret, { expiresIn: '1h' })
@@ -10,8 +16,8 @@ async function verifyJWT(token: string): Promise<{ id: string; email: string; }>
   try {
     const payload = jwt.verify(token, jwtSecret);
 
-    if (typeof payload === 'object' && 'id' in payload && 'email' in payload) {
-        return payload as { id: string, email: string };
+    if (typeof payload === 'object' && 'id' in payload && 'email' in payload && 'role' in payload) {
+        return payload as JwtPayload;
     }
     throw new Error('invalid token payload');
 
