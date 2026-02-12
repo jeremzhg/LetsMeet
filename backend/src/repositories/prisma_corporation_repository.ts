f6c821aa3a2f240fb813f5b8b4c72d58334e4f05
Jeremy <jer.auriel@gmail.com>
2026-02-12 08:31:00 +0700

    feat(auth): prisma corp repo

---
+++ backend/src/repositories/prisma_corporation_repository.ts
@@ -0,0 +1,45 @@
+import { PrismaClient, Prisma, Corporation } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const connStr = process.env.DATABASE_URL;
+const pool = new pg.Pool({connectionString: connStr});
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({adapter});
+
+async function findcorpByEmail(email: string): Promise<Corporation | null>{
+  return await prisma.corporation.findUnique({
+    where: {email},
+  });
+};
+
+async function createcorp(data: Prisma.CorporationCreateInput): Promise<Corporation>{
+  return await prisma.corporation.create({
+    data,
+  });
+};
+
+async function findcorpById (id: string): Promise<Corporation | null>{
+  return await prisma.corporation.findUnique({
+    where: {id},
+  });
+};
+
+async function claimCorp(
+  id: string, 
+  data: { 
+    hashedPassword: string; 
+    name: string; 
+    details: string 
+  }
+): Promise<Corporation>{
+  return await prisma.corporation.update({
+    where: {id},
+    data: {
+      isClaimed: true,
+      hashedPassword: data.hashedPassword,
+      name: data.name,
+      details: data.details,
+    },
+  });
+};
