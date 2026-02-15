78e439d024feaa7e7ef6350d9e7a3058bdf0caf1
jetasunanda <sunandajeta@gmail.com>
2026-02-16 00:38:25 +0700

    feat(components): enhance GuestNavbar to dynamically report height and update layout

--- frontend/src/components/GuestNavbar.jsx
+++ frontend/src/components/GuestNavbar.jsx
@@ -1,29 +1,38 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
 
-const GuestNavbar = () => {
+import { useRef, useEffect } from "react";
+
+const GuestNavbar = ({ onHeightChange }) => {
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    if (navRef.current) {
+      const height = navRef.current.offsetHeight;
+      onHeightChange(height);
+    }
+  }, [onHeightChange]);
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary" fixed="top" ref={navRef}>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">LetsMeet</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="d-flex me-auto ms-auto">
+            <Nav.Link href="#events">Find Events</Nav.Link>
+            <Nav.Link href="#sponsor">Browse Sponsors</Nav.Link>
+            <Nav.Link href="#about">About Us</Nav.Link>
+          </Nav>
+          <Nav className="d-flex align-items-center gap-2">
+            <Nav.Link href="#login" className="me-2">
+              Login
+            </Nav.Link>
+            <Button variant="primary" href="#register">
+              Register
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
