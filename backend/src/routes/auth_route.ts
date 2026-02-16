d67f1b2ac097c91072940a856482669f9acc47f1
Jeremy <jer.auriel@gmail.com>
2026-02-16 10:16:07 +0700

    feat: auth routes

--- backend/src/routes/auth_route.ts
+++ backend/src/routes/auth_route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { logout } from "../controllers/auth_controller";
+import { registerCorp, corpLogin } from "../controllers/corp_auth_controller";
 import { registerOrg, orgLogin } from "../controllers/org_auth_controller";
 
 const router = Router();
 
+router.post("/corp/register", registerCorp)
+router.post("/corp/login", corpLogin)
 router.post("/org/register", registerOrg)
 router.post("/org/login", orgLogin)
-router.post("/org/logout", logout)
+router.post("/logout", logout)
