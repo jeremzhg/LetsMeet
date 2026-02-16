00bf5f88049f52c1bb45b0deaecb4b438e92b781
Jeremy <jer.auriel@gmail.com>
2026-02-16 10:13:27 +0700

    style: camelcase function name

--- backend/src/controllers/auth_controller.ts
+++ backend/src/controllers/auth_controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 
-async function Logout(req: Request, res: Response){
+async function logout(req: Request, res: Response){
   res.clearCookie('access_token');
   return res.status(200).json({ message: 'logout successful' });
 };
 
-export {Logout}
+export {logout}
---
+++ backend/src/routes/org_auth_route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { Logout } from "../controllers/auth_controller";
+import { registerOrg, orgLogin } from "../controllers/org_auth_controller";
+
+const router = Router();
+
+router.post("/org/register", registerOrg)
+router.post("/org/login", orgLogin)
+router.post("/org/logout", Logout)
