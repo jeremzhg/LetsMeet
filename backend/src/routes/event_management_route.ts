661ca882d98ef7abc7a8eff318241299ebbc0375
Jeremy <jer.auriel@gmail.com>
2026-03-20 11:19:15 +0700

    feat: event owner validation

--- backend/src/routes/event_management_route.ts
+++ backend/src/routes/event_management_route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth_middleware';
+import { isEventOwner } from '../middleware/event_owner_middleware';
 import * as EventManagementController from '../controllers/event_management_controller';
 
 const router = Router();
@@ -12,7 +13,7 @@ router.get('/org/:userID/events', EventManagementController.getOrgEvents);
 router.post('/org/events', authMiddleware, EventManagementController.createEvent);
 router.get('/org/events/:id', EventManagementController.getEventById);
 router.get('/org/events/:id/partners', EventManagementController.getEventPartners);
-router.put('/org/events/:id', authMiddleware, EventManagementController.updateEvent);
+router.put('/org/events/:id', authMiddleware, isEventOwner, EventManagementController.updateEvent);
 
 // corp routes
 router.get('/corp/:userID/events', EventManagementController.getCorpEvents);
