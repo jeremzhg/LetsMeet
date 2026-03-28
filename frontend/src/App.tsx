382269134fe813b0894edc546fb9adad2b2ce772
jetasunanda <sunandajeta@gmail.com>
2026-03-28 12:13:28 +0700

    feat: implement authentication UI with login page, reusable form components, and routing setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
+import { HomePage } from "./pages/HomePage";
 
-export const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+  export const App = () => {
+    return (
+      <BrowserRouter>
+        <Routes>
+          {/* Default Route, otomatis lempar ke login */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          {/* Halaman utama setelah berhasil masuk */}
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  };
