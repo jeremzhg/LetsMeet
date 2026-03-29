d00dc4d0bf15a97780e426fb66dde07d4420d7cb
jetasunanda <sunandajeta@gmail.com>
2026-03-29 22:46:34 +0700

    feat: initialize authentication routing and implement login page functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,18 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { HomePage } from "./pages/HomePage";
+import { SelectRolePage } from "./pages/SelectRolePage";
 
-  export const App = () => {
-    return (
-      <BrowserRouter>
-        <Routes>
-          {/* Default Route, otomatis lempar ke login */}
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          {/* Halaman utama setelah berhasil masuk */}
-          <Route path="/home" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  };
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/select-role" element={<SelectRolePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
