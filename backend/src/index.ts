8ef2cef9c5aa99870b18bc5b734029927b503e32
Jeremy <jer.auriel@gmail.com>
2026-04-24 18:18:21 +0700

    chore: allow cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,12 +8,17 @@ import { MatchingRouter } from "./routes/matching_route"
 import cookieParser from "cookie-parser"
 import swaggerUi from "swagger-ui-express"
 import swaggerDocument from "./swagger.json"
+import cors from "cors"
 
 connectDB()
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
 const port = 3000;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
